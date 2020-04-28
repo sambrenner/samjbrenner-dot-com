388d65980eba0dcbcfb04c25770e22492e26a3ee
Sam Brenner <samjbrenner@gmail.com>
2020-04-28 14:59:28 -0400

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
@@ -45,6 +44,12 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [`gatsby-remark-highlight.js`]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/images/favicon.gif'
       }
     }
 
